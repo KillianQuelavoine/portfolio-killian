cbb6e1c3cf24f7247c2606f7201a03b3d3143c0b
Killian Quelavoine <95000365+KillianQuelavoine@users.noreply.github.com>
2026-08-19 14:09:53 +0200

    Add files via upload

---
+++ works/videos-additions.js
@@ -0,0 +1,12 @@
+window.WORK_VIDEOS.push({
+  id: "MTBtlu6ekxs",
+  title: "Trailer finales de Copa Sudamericana & Copa Libertadores",
+  channel: "ZACK",
+  views: 1_500_000,
+  role: "both",
+  format: "long",
+  category: "sport",
+  duration: null,
+  publishedAt: "2025-11-20",
+  dateApproximate: false,
+});
